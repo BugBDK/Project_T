e4d80ee1799e8917cabae1f09c47553518d35dfc
Michiyo <michiexfinte@gmail.com>
2016-11-15 16:45:26 +0700

    Start Project.

--- src/A_cal_salary.java
+++ src/A_cal_salary.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class A_cal_salary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("dd");
+		System.out.println("------ This Project is Calculate Salary -------");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Employee's name : ");
+		String name = scan.nextLine();
+		System.out.print("Input Employee's Salary : ");
+		int salary = scan.nextInt();
+		System.out.print("Input Employee's sales : ");
+		int sales = scan.nextInt();
+		
+		if (sales >= 1 || sales <= 25000){
+			
+		}
 	}
 
 }
---
+++ src/Calculate.java
@@ -0,0 +1,14 @@
+
+public class Calculate {
+	private String name;
+	private int salary;
+	private int sale;
+	
+	public Calculate (String name,int salary,int sale){
+		this.name = name;
+		this.salary = salary;
+		this.sale = sale;
+	}
+	public 
+
+}
