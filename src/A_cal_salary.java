dd36afcd4f7a00e2ebc6d89528e65724f3f4433f
Michiyo <michiexfinte@gmail.com>
2016-11-15 23:54:38 +0700

    แก้ไข Master เวอร์ชั่นล่าสุด ทดสอบแล้ว

--- src/A_cal_salary.java
+++ src/A_cal_salary.java
@@ -1,42 +1,39 @@
 import java.util.Scanner;
-public class A_cal_salary {
+public class aaaa {
 
 	public static void main(String[] args) {
-		double salary ;
-		double sales ;
 		System.out.println("------ This Project is Calculate Salary -------");
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Input Employee's Name : ");
+		System.out.print("Input Employee's name : ");
 		String name = scan.nextLine();
 		System.out.print("Input Employee's Salary : ");
-		salary = scan.nextDouble();
-		while(salary < 1 ){
-			System.out.println("--Error Employee's Salary--");
-			System.out.print("Please Input Employee's Salary : ");
+		double salary = scan.nextDouble();
+		while (salary < 1){
+			System.out.print("!!!! Input Error !!! Salary must more than 0 !!!");
+			System.out.print("Input Employee's Salary : ");
 			salary = scan.nextDouble();
-			} 
-		System.out.print("Input Employee's Sales : ");
-		sales = scan.nextDouble();
-		while(sales < 1 ){
-			System.out.println("--Error Employee's Sales--");
-			System.out.print("Please Input Employee's Sales : ");
+		}
+		System.out.print("Input Employee's sales : ");
+		double sales = scan.nextDouble();
+		while (sales < 1){
+			System.out.print("!!!! Input Error !!! Sales must more than 0 !!!");
+			System.out.print("Input Employee's Sales : ");
 			sales = scan.nextDouble();
-		} 
-		double sum=0;
+		}
+		
+		double sum;
 		Calculate cal = new Calculate(name,salary,sales);
-		if (sales >= 1 && sales <= 50000){ //minor change of Project A 
+		if (sales >= 1 && sales <= 25000){
+			sum = cal.cal_sale_less_25000();
+		}
+		else if (sales >= 25001 && sales <= 50000){
 			sum = cal.cal_sale_less_50000();
 		}
-		else if (sales <= 100000){
-			sum = cal.cal_sale_less_100000();
-		}
 		else{
-			sum = cal.cal_sale_more_100001();
+			sum = cal.cal_sale_more_50001();
 			}
-		System.out.println("");
 		System.out.println("---------------Result----------------");
 		System.out.println("Employee's name = " +name);
 		System.out.println("result = " +sum + " Bath");
 		}
 	}
-
--- src/Calculate.java
+++ src/Calculate.java
@@ -9,19 +9,20 @@ public class Calculate {
 		this.salary = salary;
 		this.sale = sale;
 	}
-	public double cal_sale_less_50000(){
+	public double cal_sale_less_25000(){
 		double sum = 0;
 		sum = this.salary+(this.sale*0.01); 
 		return sum;
 	}
-	public double cal_sale_less_100000(){
+	public double cal_sale_less_50000(){
 		double sum = 0;
 		sum = this.salary+(this.sale*0.02); 
 		return sum;
 	}
-	public double cal_sale_more_100001(){
+	public double cal_sale_more_50001(){
 		double sum = 0;
 		sum = this.salary+(this.sale*0.03); 
 		return sum;
 	}
+	
 }
